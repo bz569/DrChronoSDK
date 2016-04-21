da6e4bc5f62873f7d091b6e3043d645b8f5a67c6
Boxuan Zhang <boxuan.zhang@drchrono.com>
2016-04-20 17:04:38 -0700

    Initial Commit

---
+++ DrChronoSDK/DrChronoSDK.h
@@ -0,0 +1,19 @@
+//
+//  DrChronoSDK.h
+//  DrChronoSDK
+//
+//  Created by Boxuan Zhang on 4/20/16.
+//  Copyright © 2016 drchrono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DrChronoSDK.
+FOUNDATION_EXPORT double DrChronoSDKVersionNumber;
+
+//! Project version string for DrChronoSDK.
+FOUNDATION_EXPORT const unsigned char DrChronoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrChronoSDK/PublicHeader.h>
+
+
